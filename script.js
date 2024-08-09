ba823f5873fa488fbcbf37f4d85f4ef1e2de8311
Phelipe Sete <71620144+phelipesete@users.noreply.github.com>
2024-08-09 14:05:45 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Developer", "Designer", "Freelancer", "Programmer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        cursorChar: ' |',
+        smartBackspace: true,
+        loop: true
+    });
